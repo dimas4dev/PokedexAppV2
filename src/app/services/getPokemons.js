7aa5477bb59b64a9c7949915ecc5be04b16a797e
Dimas Mendoza <damendoza98@gmail.com>
2022-02-18 12:19:29 -0500

    Este es un cambio chido

--- src/app/services/getPokemons.js
+++ src/app/services/getPokemons.js
@@ -26,7 +26,7 @@ export const getPokemons = async () => {
    * Hacer una peticion a la API de pokeapi para obtener todos los pokemons con su informacion realizandolo con Axios
    */
 
-  const request = await axios.get(URL);
+  const request = await axios.get(URL); //Bro este es un cambio chido para mostrar si funciona git flow
   const data = request.data.results;
   const links = data.map((elements) => elements.url);
   const getPromisePokemon = links.map((link) => axios.get(link));
