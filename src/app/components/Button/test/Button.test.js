73a92c7858bab5b684e28403d9cbc88a3ce79250
SuaferoanTJK <suarezroanfelipe@gmail.com>
2022-02-24 16:19:58 -0500

    Remove texts

--- src/app/components/Button/test/Button.test.js
+++ src/app/components/Button/test/Button.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { mount, shallow } from "enzyme";
+import { shallow } from "enzyme";
 import Button from "../Button";
 
 describe("unit tests Button", () => {
@@ -15,15 +15,4 @@ describe("unit tests Button", () => {
     );
     expect(wrapper).toMatchSnapshot();
   });
-  // it("It should render 'Add' if prop.textButton is 'Add'  ", () => {
-  //   const wrapper = shallow(
-  //     <Button
-  //       textButton="Add"
-  //       char={{ id: 1, name: "bulbasaur" }}
-  //       addPokemons={() => {}}
-  //       removePokemons={() => {}}
-  //     />
-  //   );
-  //   expect(wrapper.find("button").at(0).text()).toEqual("Add");
-  // });
 });
